d607a2e01cbc56067fd166026450f2a88d873c62
mac16 <1798996525@qq.com>
2025-08-25 21:55:01 +0800

    note

---
+++ src/main/java/org/datastructure/api/StackApi.java
@@ -0,0 +1,27 @@
+package org.datastructure.api;
+
+import java.util.Stack;
+
+/**
+ * 栈  后进 先出
+ */
+public class StackApi {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        //入栈
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        //获取栈顶元素 = 3
+        System.out.println(stack.peek());
+
+
+        //出栈 3 2 1
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
