c9457aff308a17c997c4ac8c4f174846a36bdf22
mac16 <1798996525@qq.com>
2025-09-07 22:20:07 +0800

    note

--- src/main/java/org/datastructure/api/TreeSetApi.java
+++ src/main/java/org/datastructure/api/TreeSetApi.java
@@ -14,26 +14,43 @@ public class TreeSetApi {
         treeSet.add(2);
         //删除
         treeSet.remove(1);
-        //找出【大于】入参的最小值
+
+        /**
+         * 找出【大于】入参的最小值
+         */
         Integer higher = treeSet.higher(0);
-        //找出【小于】入参的最大值
+        /**
+         * 找出【小于】入参的最大值
+         */
         Integer lower = treeSet.lower(0);
-        //天花板，找出【大于等于】入参的最小值
+        /**
+         * 天花板，找出【大于等于】入参的最小值
+         */
         Integer ceiling = treeSet.ceiling(0);
 
-        //地板，找出【小于等于】入参的最大值
+        /**
+         * 地板，找出【小于等于】入参的最大值
+         */
         Integer floor = treeSet.floor(0);
 
-        //获取【最小值】
+        /**
+         * 获取【最小值】
+         */
         Integer first = treeSet.first();
 
-        //获取【最大值】
+        /**
+         * 获取【最大值】
+         */
         Integer last = treeSet.last();
 
-        //获取【最小值】并删除
+        /**
+         * 获取【最小值】并删除
+         */
         Integer i = treeSet.pollFirst();
 
-        //获取【最大值】并删除
+        /**
+         * 获取【最大值】并删除
+         */
         Integer j = treeSet.pollLast();
     }
 }
