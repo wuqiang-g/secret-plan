56afe608a69b477c0bc5b5a52fef4100ad7cfa4e
baici.wq <baici.wq@alibaba-inc.com>
2025-10-11 11:04:08 +0800

    note

--- src/main/java/org/datastructure/graph/GraphDemo.java
+++ src/main/java/org/datastructure/graph/GraphDemo.java
@@ -1,5 +1,75 @@
 package org.datastructure.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 有向加权图，领接表实现
+ * 如果不用加权的话，则不用对象，用Integer代表指向节点即可
+ * 无向图的话，相当于增删的时候，多加一条边，无向图相当于双向图。
+ */
 public class GraphDemo {
 
+    //领接表,索引是当前节点编号,List<Edge> 存储当前节点指向的节点
+    //也可以Map<Integer, List<Edge>> graph;
+    List<Edge>[] graph;
+
+    //边
+    class Edge{
+        //指向
+        int to;
+        //权重
+        int wight;
+        public Edge(int to, int wight) {
+            this.to = to;
+            this.wight = wight;
+        }
+    }
+
+    public GraphDemo(int n) {
+        graph = new List[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+    }
+
+    // 增，添加一条带权重的有向边，复杂度 O(1)
+    public void addEdge(int from, int to, int weight) {
+        graph[from].add(new Edge(to, weight));
+    }
+
+    // 删，删除一条有向边，复杂度 O(V)
+    public void removeEdge(int from, int to) {
+        for(Edge edge : graph[from]) {
+            if (edge.to == to) {
+                graph[from].remove(edge);
+                break;
+            }
+        }
+    }
+
+    // 查，判断两个节点是否相邻，复杂度 O(V)
+    public boolean hasEdge(int from, int to) {
+        for(Edge edge : graph[from]) {
+            if (edge.to == to) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查，返回一条边的权重，复杂度 O(V)
+    public int weight(int from, int to) {
+        for(Edge edge : graph[from]) {
+            if (edge.to == to) {
+                return edge.wight;
+            }
+        }
+        return -1;
+    }
+
+    // 查，返回某个节点的所有邻居节点，复杂度 O(1)
+    public List<Edge> neighbors(int v) {
+        return graph[v];
+    }
 }
---
+++ src/main/java/org/datastructure/graph/GraphMatrixDemo.java
@@ -0,0 +1,58 @@
+package org.datastructure.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 有向加权图，领接矩阵实现
+ */
+public class GraphMatrixDemo {
+    // 存储相邻节点及边的权重
+    public static class Edge {
+        int to;
+        int weight;
+
+        public Edge(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    // 领接矩阵. 索引是当前节点编号,matrix[x][y] 记录 x 是否有一条指向 y 的边,值是边权重
+    private int[][] matrix;
+
+    public GraphMatrixDemo(int n) {
+        matrix = new int[n][n];
+    }
+
+    public void addEdge(int from, int to, int weight) {
+        matrix[from][to] = weight;
+    }
+
+    // 删，删除一条有向边，复杂度 O(1)
+    public void removeEdge(int from, int to) {
+        matrix[from][to] = 0;
+    }
+
+    // 查，判断两个节点是否相邻，复杂度 O(1)
+    public boolean hasEdge(int from, int to) {
+        return matrix[from][to] != 0;
+    }
+
+    // 查，返回一条边的权重，复杂度 O(1)
+    public int weight(int from, int to) {
+        return matrix[from][to];
+    }
+
+    // 查，返回某个节点的所有邻居节点，复杂度 O(V)
+    public List<Edge> neighbors(int v) {
+        List<Edge> res = new ArrayList<>();
+        for (int i = 0; i < matrix[v].length; i++) {
+            if (matrix[v][i] != 0) {
+                res.add(new Edge(i, matrix[v][i]));
+            }
+        }
+        return res;
+    }
+
+}
